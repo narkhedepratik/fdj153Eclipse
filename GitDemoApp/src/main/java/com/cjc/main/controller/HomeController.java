7f2d16897ce7c8ef674912b8dae033bb4cd3ac68
narkhedepratik <98662996+narkhedepratik@users.noreply.github.com>
2023-06-14 10:48:23 +0530

    I am dev 2

--- GitDemoApp/src/main/java/com/cjc/main/controller/HomeController.java
+++ GitDemoApp/src/main/java/com/cjc/main/controller/HomeController.java
@@ -9,6 +9,14 @@ public class HomeController {
 	@GetMapping("/msg")
 	public String getMsg()
 	{
+		
 		return "I am Dev 1";
+	}
+	
+	@GetMapping("/msg2")
+	public String getMsg2()
+	{
+		
+		return "I am Dev 2";
 	}
 }
